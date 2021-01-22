e4ba4b2ddad01349ec7edec99627378341d765f5
Radek Tobolka <tobolka@gmail.com>
2021-01-22 01:29:53 +0100

    chart init

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,56 @@
 import Head from "next/head";
+import {
+  AreaChart,
+  Area,
+  Tooltip,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+} from "recharts";
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 export default function Home() {
   return (
@@ -83,7 +135,7 @@ export default function Home() {
 
         <div className="flex mt-14">
           {/* Widget 1 */}
-          <div className="bg-white p-4 rounded-md shadow w-72">
+          <div className="bg-white p-4 rounded-md shadow">
             <div className="text-blueGray-600 font-light mb-2">Sessions</div>
             <div className="mb-2">
               <span className="text-2xl font-medium number">$630.44</span>
@@ -106,8 +158,38 @@ export default function Home() {
                 93.9%
               </span>
             </div>
-            <div className="text-xs text-blueGray-400 font-light number">
+            <div className="text-xs text-blueGray-400 font-light number mb-6">
               $2,134,124 same period last month
+            </div>
+            <div>
+              <AreaChart
+                width={560}
+                height={250}
+                data={data}
+                margin={{ top: 4, right: 4, left: -20, bottom: 0 }}
+              >
+                <defs>
+                  <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#6366F1" stopOpacity={0.8} />
+                    <stop offset="95%" stopColor="#6366F1" stopOpacity={0} />
+                  </linearGradient>
+                </defs>
+                <XAxis stroke="#94A3B8" fontSize={10} axisLine={false} tickLine={false} />
+                <YAxis stroke="#94A3B8" fontSize={10} axisLine={false} tickLine={false} />
+                <CartesianGrid
+                  strokeDasharray="1 2"
+                  vertical={false}
+                  stroke="#CBD5E1"
+                />
+                <Tooltip cursor={{ stroke: '#E2E8F0',}} />
+                <Area
+                  type="monotone"
+                  dataKey="uv"
+                  stroke="#6366F1"
+                  fillOpacity={1}
+                  fill="url(#colorUv)"
+                />
+              </AreaChart>
             </div>
           </div>
         </div>
